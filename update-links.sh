9473828f152ea12c8bea823a2d6f56600ef2f2a6
Tamer Saadeh <tamersaadeh@gmail.com>
2013-12-14 23:33:54 +0100

    fixed for loop syntax in the update-links script

--- update-links.sh
+++ update-links.sh
@@ -3,7 +3,7 @@
 BASE=`pwd`
 DEST="/etc"
 
-for y in {init, cron}; do
+for y in init cron; do
 	for file in $( ls ); do
 		sudo ln -s "$BASE/$y"/"$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
 	done
