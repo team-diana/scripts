082e93bb22834f4397b009689f6dcd52f9fd154f
Vincenzo Comito <clynamen@gmail.com>
2013-12-15 01:21:16 +0100

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,77 @@
+
+function okMsg {
+  echo -e "\e[1;32m $@ \e[0m"
+}
+
+function warningMsg {
+  echo -e "\e[1;33m $@ \e[0m"
+}
+
+
+function errorMsg {
+  echo -e "\e[1;31m $@ \e[0m"
+  exit -1
+}
+
+function exitOnNo {
+  ans=""
+  read ans
+
+  if [[ $ans != "y" ]]; then
+    exit -1
+  fi
+}
+
+echo "This script will do a complete setup of the rover. It will download git repos and config files"
+warningMsg "WARNING: this mean that files will be overwritten! backup your old files if not sure"
+echo "Continue? [y/N]"
+
+exitOnNo
+
+echo "Moving to home directory..."
+cd ~
+
+echo "Setting up git"
+wget https://raw.github.com/team-diana/dotfiles/master/git/gitconfig.symlink -O ~/.gitconfig
+okMsg "Done"
+
+echo "Downloading the script repo..."
+git clone https://github.com/team-diana/scripts.git  || errorMsg "Unable to retrieve scripts from git"
+okMsg "Download done"
+
+echo "Downloading the catkin_ws repo..."
+git clone https://github.com/team-diana/catkin_ws.git  || errorMsg "Unable to retrieve the catkin_ws repo from git"
+echo "Init submodules"
+cd catkin_ws 
+git submodule init && git submodule update || errorMsg "Unable to retrieve submodules"
+cd ..
+okMsg "Download done"
+
+echo "Install all the needed stuff... (Requires root permission) [Y/n]"
+ans
+read ans
+
+if [[ $ans != "n" ]]; then
+  #TODO: add all the needed libs
+  packages = "cmake"
+  sudo apt-get install $packages || errorMsg "Unable to install packages"
+  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+  wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+  sudo apt-get install ros-groovy-desktop-full
+  sudo rosdep init
+  rosdep update
+  sudo apt-get install python-rosinstall
+fi
+
+echo "Copying init files... (Requires root permission)"
+sudo bash ~/scripts/update-links.sh ||  errorMsg "Unable to install init files"
+okMsg "Init files copied"
+
+cd ~
+echo "Downloading the dotfiles repo repo..."
+git clone https://github.com/team-diana/dotfiles.git || errorMsg "Unable to retrieve scriptsles.git from git"
+echo "Starting dotfiles bootstrap..."
+bash ~/.dotfiles/script/bootstrap
+okMsg "dotfiles copied"
+
+okMsg "SETUP COMPLETED"
