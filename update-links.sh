1e519a051a60dcea8d5d2e7a01fea5388afffb1c
Tamer Saadeh <tamersaadeh@gmail.com>
2013-12-20 18:11:18 +0100

    fixed a problem with the linknig script for cron and init

--- update-links.sh
+++ update-links.sh
@@ -4,7 +4,9 @@ BASE=`pwd`
 DEST="/etc"
 
 for y in init cron; do
-	for file in $( ls ); do
-		sudo ln -s "$BASE/$y"/"$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+	for file in $( cd "$BASE/$y" && ls ); do
+        if [[ "$file" != "README.md" ]]; then
+            sudo ln -s "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+        fi
 	done
 done
