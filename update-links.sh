70aec9048e15db70f3d6b690c426cdee285ef03b
Tamer Saadeh <tamersaadeh@gmail.com>
2013-12-20 18:13:13 +0100

    fixed wrong path for cron->cron.d

--- update-links.sh
+++ update-links.sh
@@ -3,7 +3,7 @@
 BASE=`pwd`
 DEST="/etc"
 
-for y in init cron; do
+for y in "init" "cron.d"; do
 	for file in $( cd "$BASE/$y" && ls ); do
         if [[ "$file" != "README.md" ]]; then
             sudo ln -s "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
