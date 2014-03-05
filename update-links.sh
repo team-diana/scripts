44a3e0bf242723293de83e3d84f9b59bbea311d1
Tamer Saadeh <tamersaadeh@gmail.com>
2014-03-05 17:32:02 +0100

    fix the cron and init scripts (I'm assuming that they should be ownded by root)

--- update-links.sh
+++ update-links.sh
@@ -6,7 +6,8 @@ DEST="/etc"
 for y in "init" "cron.d"; do
 	for file in $( cd "$BASE/$y" && ls ); do
         if [[ "$file" != "README.md" ]]; then
-            sudo ln -s "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+            sudo cp "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+            sudo chown root:root"$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
         fi
 	done
 done
