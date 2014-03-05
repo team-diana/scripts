51bf38f5b0a6589151c1edf9e5fb469852b5d032
Tamer Saadeh <tamersaadeh@gmail.com>
2014-03-05 17:33:52 +0100

    fix typo

--- update-links.sh
+++ update-links.sh
@@ -7,7 +7,8 @@ for y in "init" "cron.d"; do
 	for file in $( cd "$BASE/$y" && ls ); do
         if [[ "$file" != "README.md" ]]; then
             sudo cp "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
-            sudo chown root:root"$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+            sudo chown root:root "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+            sudo chmod +x "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
         fi
 	done
 done
