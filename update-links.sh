ade848297f6942cc8dfe4646e206c3ca3f333c2f
Tamer Saadeh <tamersaadeh@gmail.com>
2014-03-06 23:49:08 +0100

    typo 2: i need to sleep

--- update-links.sh
+++ update-links.sh
@@ -10,7 +10,7 @@ for y in "init" "cron.d"; do
             sudo cp "$BASE/$y/$file" "$TO" 2>&1 > /dev/null
             sudo chown root:root "$TO" 2>&1 > /dev/null
             sudo chmod +x "$TO" 2>&1 > /dev/null
-            if [[ "$y" == "cron.d" ]]; do
+            if [[ "$y" == "cron.d" ]]; then
                 crontab "$TO"
             fi
         fi
