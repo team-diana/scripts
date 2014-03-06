1d3515721177bf4e60a393ac609aebd731acc7d2
Tamer Saadeh <tamersaadeh@gmail.com>
2014-03-06 23:41:13 +0100

    reboot?

--- update-links.sh
+++ update-links.sh
@@ -6,9 +6,11 @@ DEST="/etc"
 for y in "init" "cron.d"; do
 	for file in $( cd "$BASE/$y" && ls ); do
         if [[ "$file" != "README.md" ]]; then
-            sudo cp "$BASE/$y/$file" "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
-            sudo chown root:root "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
-            sudo chmod +x "$DEST/$y"/team-diana-"$file" 2>&1 > /dev/null
+            TO="$DEST/$y"/team-diana-"$file"
+            sudo cp "$BASE/$y/$file" "$TO" 2>&1 > /dev/null
+            sudo chown root:root "$TO 2>&1 > /dev/null
+            sudo chmod +x "$TO" 2>&1 > /dev/null
+            crontab "$TO"
         fi
 	done
 done
